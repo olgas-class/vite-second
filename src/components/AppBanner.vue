2bf383bd8736f6912181a01eab3abbc6448196e8
olga_demina <olga.demina1410@gmail.com>
2024-04-30 13:07:28 +0200

    feat: components example

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "My banner title",
+      image: "milano.jpg",
+    };
+  },
+  methods: {
+    getImagePath(imageName) {
+      return new URL(`../assets/img/${imageName}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="banner">
+    <h2>{{ title }}</h2>
+    <img :src="getImagePath(image)" alt="" />
+  </div>
+</template>
+
+<style>
+.banner {
+  position: relative;
+}
+
+.banner img {
+  width: 100%;
+}
+
+.banner h2 {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  background-color: rgba(0, 0, 0, 0.4);
+  color: white;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "My nice title"
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+
+</template>
+
+<style></style>
